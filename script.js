a8fcd9cf3d0a42a8142c8b4c50519382858ba406
EdwardNicolasHaddad <eeex1x2@gmail.com>
2026-07-12 16:19:00 +0200

    Add async function to check user code and role

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+async function checkCode() {
 
+    let eingabe = document.getElementById("code").value;
+
+    let { data, error } = await supabaseClient
+        .from("codes")
+        .select("*")
+        .eq("code", eingabe)
+        .single();
+
+
+    if (error || !data) {
+
+        alert("Falscher Code");
+
+        return;
+    }
+
+
+    if (data.role === "admin") {
+
+        window.location.href = "admin.html";
+
+    }
+
+
+    else if (data.role === "besucher") {
+
+        window.location.href = "besucher.html";
+
+    }
+
+}
