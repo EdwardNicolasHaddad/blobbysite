a53e47e99e5ab3eb3824f2c1194385f69c0246ef
EdwardNicolasHaddad <eeex1x2@gmail.com>
2026-07-13 19:49:20 +0200

    Refactor like button to include onclick handler

--- besucher.js
+++ besucher.js
@@ -70,7 +70,10 @@ async function loadBlocks() {
 
     <div class="like-area">
 
-        <button class="like-button ${liked ? "liked" : ""}">
+        <button 
+            class="like-button ${liked ? "liked" : ""}"
+            onclick="toggleLike(${block.id})"
+        >
 
             <svg viewBox="0 0 24 24">
                 <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 
