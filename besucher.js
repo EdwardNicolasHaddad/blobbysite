3e9d0d39e6ce43d37dd1acf872cd6376c24416a7
EdwardNicolasHaddad <eeex1x2@gmail.com>
2026-07-13 19:07:55 +0200

    Replace like button with SVG icon

--- besucher.js
+++ besucher.js
@@ -41,9 +41,13 @@ async function loadBlocks() {
     <div class="like-area">
 
         <button class="like-button">
-            ♡
-        </button>
 
+            <svg viewBox="0 0 24 24">
+                <path d="M12 21s-7-4.35-9.33-8.33C.67 9.35 2.1 5 5.5 5c1.74 0 3.41 1 4.5 2.09C11.09 6 12.76 5 14.5 5 17.9 5 19.33 9.35 21.33 12.67 19 16.65 12 21 12 21z"/>
+            </svg>
+
+        </button>
+        
         <span>
             ${block.likes || 0}
         </span>
