a60b6be7e239045ef7925c5ad2181a7acefdad53
EdwardNicolasHaddad <eeex1x2@gmail.com>
2026-07-13 19:24:58 +0200

    Implement getDeviceId function
    Add function to retrieve or generate device ID

--- besucher.js
+++ besucher.js
@@ -1,3 +1,19 @@
+function getDeviceId(){
+
+    let deviceId = localStorage.getItem("deviceId");
+
+    if(!deviceId){
+
+        deviceId = crypto.randomUUID();
+
+        localStorage.setItem("deviceId", deviceId);
+
+    }
+
+    return deviceId;
+
+}
+
 async function loadBlocks() {
 
     let { data, error } = await supabaseClient
